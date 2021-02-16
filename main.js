281dc41cc5bc66e6267350f68dd6e3cf9321ab57
Franklin <franklinokolie86@gmail.com>
2021-02-16 10:56:42 +0100

    Added image slider function

---
+++ main.js
@@ -0,0 +1,29 @@
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+	 
+// Image List
+images[0] = "images/5.png";
+images[1] = "images/6.png";
+images[2] = "images/7.png";
+//images[3] = "http://lorempixel.com/400/200/people";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
